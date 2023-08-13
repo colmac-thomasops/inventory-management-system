e87a4b9e4c37be62dbe0cbc448fbab0275f7615f
colmac-thomasops <alcatiz2222@gmail.com>
2023-08-13 12:43:45 +0000

    Enhancements to login and dashboardRefresh javascripts

--- assets/js/dashboardRefresh.js
+++ assets/js/dashboardRefresh.js
@@ -21,6 +21,32 @@ function updateItemCount() {
     });
 }
 
-updateItemCount();
+function updateActiveVendors() {
+    $.ajax({
+        url: 'model/dashboard/getActiveVendors.php',
+        method: 'GET',
+        dataType: 'text',
+        success: function(response) {
+            try {
+                const jsonResponse = JSON.parse(response);
+                if (jsonResponse.success) {
+                    $('#activeVendorCountContainer').text(jsonResponse.activeVendors);
+                } else {
+                    console.error('Error fetching item count:', jsonResponse.error);
+                }
+            } catch (error) {
+                console.error('Error parsing JSON response:', error);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('Error fetching item count:', textStatus, errorThrown);
+        }
+    });
+}
 
-setInterval(updateItemCount, 10000) // 10000 milliseconds = 10 seconds
+// Call the intial functions for dashboard refresh
+updateItemCount();
+updateActiveVendors();
+
+setInterval(updateItemCount, 10000); // 10000 milliseconds = 10 seconds
+setInterval(updateActiveVendors, 10000);
