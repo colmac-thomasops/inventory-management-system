e8997723f303ed9616334a1a0bf930825ff66816
colmac-thomasops <alcatiz2222@gmail.com>
2023-08-13 15:22:41 +0000

    Updated dashboard with refreshing essential metrics

--- model/dashboard/getItemOverallSoldCount.php
+++ model/dashboard/getItemOverallSoldCount.php
@@ -2,11 +2,5 @@
     require_once (__DIR__ . '/controller/getItemCountController.php');
     $controller = new GetItemCountController();
     $response = $controller->getOverallProductSoldCount();
-    $jsonResponse = json_decode($response, true);
-
-    if ($jsonResponse["success"] == true && isset($jsonResponse["success"])) {
-        echo $jsonResponse["itemOverallSoldCount"];
-    } else {
-        echo $jsonResponse["error"];
-    }
+    echo $response;
 ?>
