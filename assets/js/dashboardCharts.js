7d6d8d2440d6fecb5a245956e4baf9d7514cd8fe
colmac-thomasops <alcatiz2222@gmail.com>
2023-08-27 16:47:25 +0000

    Updated code to render area chart properly

--- assets/js/dashboardCharts.js
+++ assets/js/dashboardCharts.js
@@ -1,5 +1,6 @@
 var salesOrdersChartName = 'Sales Orders';
 var purchaseOrdersChartName = 'Purchase Orders';
+var monthsXAxis = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"];
 
 var areaChartOptionsPurchaseNSales = {
     chart: {
@@ -17,31 +18,54 @@ var areaChartOptionsPurchaseNSales = {
             data: []
         }
     ],
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    xaxis: {
+        categories: monthsXAxis
+    },
     dataLabels: {
         enabled: false
+    },
+    fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.7,
+          opacityTo: 0.9,
+          stops: [0, 90, 100]
+        }
     }
 }
 
-var areaChartPurchaseNSales = new ApexCharts(
+/*var areaChartPurchaseNSales = new ApexCharts(
     document.querySelector('#area-chart'),
     areaChartOptionsPurchaseNSales
-)
-areaChartPurchaseNSales.render();
+);*/
 
-function updateAreaChart() {
-    $.ajax({
-        url: 'model/dashboard/getItemWeeklySoldCount.php',
-        method: 'GET',
-        success: function(response) {
-            try {
-                const jsonResponse = JSON.parse(response);
-                console.log(jsonResponse);
-                areaChartPurchaseNSales.updateSeries([{name: salesOrdersChartName, data: jsonResponse.itemsSold}], true);
 
-            } catch (error) {
-                console.log(`Error fetching JSON data: ${error}`);
+$(document).ready(function() {
+    function updateAreaChart() {
+        $.ajax({
+            url: 'model/dashboard/getItemSoldCountperMonth.php',
+            method: 'GET',
+            success: function(response) {
+                try {
+                    const jsonResponse = JSON.parse(response);
+                    console.log(jsonResponse);
+    
+                    const itemsSoldData = jsonResponse.perMonthSoldCount.map(item => item.itemsSold);
+    
+                    areaChartPurchaseNSales.updateSeries([{name: salesOrdersChartName, data: itemsSoldData}], true);
+    
+                } catch (error) {
+                    console.log(`Error fetching JSON data: ${error}`);
+                }
             }
-        }
-    });
-}
+        });
+    }
+    
+    var areaChartPurchaseNSales = new ApexCharts(
+        document.querySelector('#area-chart'),
+        areaChartOptionsPurchaseNSales
+    );
+    updateAreaChart();
+    areaChartPurchaseNSales.render();
+});
